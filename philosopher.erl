ccd0bdaaea5fc3e08e8e90ee76f2d144f4a62a25
Eoin Nugent <enugent@hmc.edu>
2014-03-11 23:58:54 -0700

    Completed the code, testStrategy, and readme.

--- philosopher.erl
+++ philosopher.erl
@@ -1,67 +1,38 @@
-% TODO: HANDLE REQUEST FORK AND CREATEFORK IN THINKING, HUNGRY, EATING.
-
-
 -module(philosopher).
 -export([main/1]).
 -define(TIMEOUT, 2000).
 
 % NOTE: a fork is defined as a tuple with two neighbours and an isClean boolean
-% neighbour1 and neighbour2 are encoded via each one's pid.
+% neighbour1 and neighbour2 are encoded via each one's node name, as an atom.
 
-%main driver of application
 main(Params) ->
-		% try
-				%set up network connections
-				_ = os:cmd("epmd -daemon"),
-				Reg_name = hd(Params),
-				Neighbours = lists:map(fun(X) -> list_to_atom(X) end, tl(Params)), 
-				% taken as list of neighbours, converted to atoms
-				io:format("Neighbours are ~p~n", [Neighbours]),
-				net_kernel:start([list_to_atom(Reg_name), shortnames]),
-				register(philosopher, self()),
-				io:format("Registered as ~p at node ~p, currently joining. ~p~n",
-								  [philosopher, node(), now()]),
-				joinState(length(Neighbours), [], Neighbours),
-				% handleMessage(joining, 0, [], Neighbours)
-		%catch
-		%		_:_ -> io:format("Error parsing command line parameters.~n")
-		%end,
+		%set up network connections
+		_ = os:cmd("epmd -daemon"),
+		Reg_name = hd(Params),
+		Neighbours = lists:map(fun(X) -> list_to_atom(X) end, tl(Params)), 
+		% taken as list of neighbours, converted to atoms
+		io:format("Neighbours are ~p~p~n", [Neighbours, now()]),
+		net_kernel:start([list_to_atom(Reg_name), shortnames]),
+		register(philosopher, self()),
+		io:format("Registered as ~p at node ~p, currently joining. ~p~n",
+						  [philosopher, node(), now()]),
+		joinState(length(Neighbours), [], Neighbours),
 		halt().
-
-% %handles all messages and does so recursively
-% handleMessage(State, NumForksNeeded, Forks, Neighbours) ->
-% 		case State of
-% 				joining     -> joinState(NumForksNeeded, Forks, Neighbours);
-% 				thinking    -> thinkingState(NumForksNeeded, Forks, Neighbours);
-% 				hungry      -> hungryState(NumForksNeeded, Forks, Neighbours);
-% 				eating      -> eatingState(NumForksNeeded, Forks, Neighbours);
-% 				leaving     -> leavingState(NumForksNeeded, Forks, Neighbours); 
-% 				% Leaving and gone will be really awkward to do if we don't pass them directly.
-% 				% I think it'll be better if we transition directly between states by 
-% 				% calling their respective functions...
-% 				gone        -> goneState(NumForksNeeded, Forks, Neighbours);
-% 				_Else       -> io:format("Error parsing state. ~n")
-% 		end.
 
 % Sends identify requests to all neighbours. Receives their responses.
 % Tells hungry, thinking, or eating nodes to create forks with it.
 % Otherwise, it will join the network once it has the lowest pid of its joining
 % neighbours.
-
-% TODO: We need to update numforksneeded when we sendCreateFork()
 joinState(NumForksNeeded, Forks, Neighbours) -> 
-		io:format("in joining! ~n"),
-		io:format("Neighbours are ~p~n", [Neighbours]),
-		CanStartThinking = sendIdentifyMessage(Forks, Neighbours, 1),
+		io:format("In joining state ~p~n", [now()]),
+		CanStartThinking = sendIdentifyMessage(Forks, Neighbours, 1), % identifies neighbours
 		if 
-				CanStartThinking == true -> 
-						io:format("About to become thinking~n"), 
+				CanStartThinking == true -> % we have lowest pid of joiners
 						thinkingState(NumForksNeeded, Forks, Neighbours);
-						% handleMessage(thinking, NumForksNeeded, Forks, Neighbours);
-				true -> io:format("nope, someone is higher than me! ~n"), 
-						receive
+				true -> % a joining node has a higher pid 
+						receive % respond to other identify requests in the meantime
 								{ClientPid, identifyRequest} -> ClientPid ! {self(), identifyResponse, joining}
-						after ?TIMEOUT -> io:format("Timed out waiting for reply!~n")
+						after ?TIMEOUT -> io:format("Timed out waiting for reply~p~n", [now()])
 						end,
 						joinState(NumForksNeeded, Forks, Neighbours)
 		end.
@@ -71,17 +42,15 @@ joinState(NumForksNeeded, Forks, Neighbours) ->
 % if it's greater than all its neighbors, stay joining. 
 % if it's the only joining node, it can become thinking.
 sendIdentifyMessage(Forks, Neighbours, CurrCount) ->
-		io:format("in sending ~n"),
+		io:format("Sending identify message ~p~n", [now()]),
 		if 
 				CurrCount =< length(Neighbours) -> 
 						ClientNodeName = lists:nth(CurrCount, Neighbours), 
-						io:format("before"),
 						{philosopher, ClientNodeName} ! {self(), identifyRequest},
-						io:format("after"),
 						receive
 								{ClientPid, identifyResponse, joining} -> 
 										if
-												ClientPid < self() -> false;
+												ClientPid < self() -> false; % if his clientpid is lower, he has precedence on joining
 												true -> sendIdentifyMessage(Forks, Neighbours, CurrCount+1)
 										end; % otherwise if it's hungry, thinking, or eating create a fork with it
 								{ClientPid, identifyResponse, thinking} -> sendCreateFork(Forks, Neighbours, ClientPid),
@@ -92,13 +61,13 @@ sendIdentifyMessage(Forks, Neighbours, CurrCount) ->
 																												   sendIdentifyMessage(Forks, Neighbours, CurrCount+1);
 								{_, identifyResponse, _} -> sendIdentifyMessage(Forks, Neighbours, CurrCount+1)       
 						end;
-				true -> true
+				true -> true % we've cleared ourselves with every neighbour
 		end.
 
 % Check if the fork already exists with that neighbor. If not, create it by
 % sending appropriate message.
 sendCreateFork(Forks, Neighbours, ClientPid) ->
-		io:format("in sendCreateFork! ~n"),
+		io:format("In sendCreateFork ~p~n", [now()]),
 		IsNeighbourInFork = checkNeighbourInFork(Forks, Neighbours),
 		if IsNeighbourInFork == false ->
 			ClientPid ! {self(), createFork}
@@ -106,9 +75,9 @@ sendCreateFork(Forks, Neighbours, ClientPid) ->
 
 % listens for messages, responds appropriately. Repeats.
 thinkingState(NumForksNeeded, Forks, Neighbours) -> 
-		io:format("got to thinking: numforks, forks, neighbours ~p~p~p~n", [NumForksNeeded, Forks, Neighbours]),
+		io:format("In thinking: numforks, forks, neighbours ~p~p~p~p~n", [NumForksNeeded, Forks, Neighbours, now()]),
 		receive
-				{ClientPid, identifyRequest} -> io:format("Identifying self as thinking~n"),
+				{ClientPid, identifyRequest} -> io:format("Identifying self as thinking~p~n", [now()]),
 																				ClientPid ! {self(), identifyResponse, thinking},
 				 																thinkingState(NumForksNeeded, Forks, Neighbours);
 				{ClientPid, createFork} -> % see if we need to create the fork. We could get multiple such requests
@@ -117,28 +86,28 @@ thinkingState(NumForksNeeded, Forks, Neighbours) ->
 																	 																													 ClientPid),
 																	 thinkingState(NewNumForksNeeded, NewForks, NewNeighbours);
 				{ClientPid, requestFork} -> % always send fork
-					io:format("Received request for fork from ~p~n", [node(ClientPid)]),
+					io:format("Received request for fork from ~p~p~n", [node(ClientPid), now()]),
 					case findFork(Forks, node(ClientPid)) of % if we have the fork, send it.
 						false -> thinkingState(NumForksNeeded, Forks, Neighbours);
 						Fork ->
-							CleanedFork = {element(1, Fork), element(2, Fork), true},
+							CleanedFork = {element(1, Fork), element(2, Fork), true}, % clean the fork
 							ClientPid ! {self(), sendFork, CleanedFork},
 							NewSetOfForks = lists:delete(Fork, Forks),
 							thinkingState(NumForksNeeded, NewSetOfForks, Neighbours)
 					end;
-				{ClientPid, Ref, become_hungry} -> io:format("Received message to become hungry!~n"),
+				{ClientPid, Ref, become_hungry} -> io:format("Received message to become hungry~p~n", [now()]),
 																	hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref);
 				{ClientPid, Ref, leave} -> leavingState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref);
-				{ClientPid, leaving} -> io:format("Received message that node ~p is leaving ~n", [node(ClientPid)]),
+				{ClientPid, leaving} -> io:format("Received message that node ~p is leaving ~p~n", [node(ClientPid), now()]),
 																[NewNumForksNeeded, NewForks, NewNeighbours] = deleteForkFromPid(NumForksNeeded, Forks, Neighbours, ClientPid),
 																thinkingState(NewNumForksNeeded, NewForks, NewNeighbours);
-				Message -> io:format("Couldn't interpret message ~p~n", [Message]) % TODO: REMOVE?
+				Message -> io:format("Couldn't interpret message ~p~p~n", [Message, now()])
 		end.
 
 % Sees if we have a fork with a given neighbour. If not, creates it and adds
-% that node to our list of neighbours.
+% that node to our list of neighbours. Returns all our new parameters.
 createFork(NumForksNeeded, Forks, Neighbours, ClientPid) ->
-	 io:format("Creating fork!~n"),
+	 io:format("Creating fork~p~n", [now()]),
 	 DoWeHaveFork = checkNeighbourInFork(Forks, node(ClientPid)),
 	 if
 	 	 DoWeHaveFork == false -> 
@@ -153,9 +122,10 @@ createFork(NumForksNeeded, Forks, Neighbours, ClientPid) ->
 % First we listen for any existing messages to identify, create, or request
 % forks from us. We do this first so that we never hold on to a fork that someone
 % else has priority on.
-hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> io:format("got to hungry~n"),
+hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> 
+		io:format("In hungry state~p~n", [now()]),
 		receive
-			{ReceiverClientPid, identifyRequest} -> io:format("Identifying self as hungry~n"),
+			{ReceiverClientPid, identifyRequest} -> io:format("Identifying self as hungry~p~n", [now()]),
 																	ReceiverClientPid ! {self(), identifyResponse, hungry},
 																	hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref);
 			{ReceiverClientPid, createFork} -> % see if we need to create the fork. We could get multiple such requests
@@ -165,13 +135,13 @@ hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> io:format("got
 															 hungryState(NewNumForksNeeded, NewForks, NewNeighbours, ClientPid, Ref);
 			{ReceiverClientPid, Ref, leave} -> leavingState(NumForksNeeded, Forks, Neighbours, ReceiverClientPid, Ref);
 			{ReceiverClientPid, leaving} ->
-																io:format("Received message that node ~p is leaving ~n", [node(ReceiverClientPid)]),
+																io:format("Received message that node ~p is leaving ~p~n", [node(ReceiverClientPid), now()]),
 																[NewNumForksNeeded, NewForks, NewNeighbours] = deleteForkFromPid(NumForksNeeded, Forks, Neighbours, ReceiverClientPid),
 																hungryState(NewNumForksNeeded, NewForks, NewNeighbours, ClientPid, Ref);
 			{ReceiverClientPid, requestFork} -> % someone wants our fork.
 																				% we must give it to them if the fork
 																				% is dirty.
-				io:format("Received request for fork from ~p~n", [node(ReceiverClientPid)]),
+				io:format("Received request for fork from ~p~p~n", [node(ReceiverClientPid), now()]),
 				case findFork(Forks, node(ClientPid)) of
 					false -> hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref);
 					Fork ->
@@ -183,7 +153,7 @@ hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> io:format("got
 							true -> hungryState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref)
 						end
 				end
-		after ?TIMEOUT -> 
+		after ?TIMEOUT -> % if we've dealt with our messages, go to eating if we have all forks
 				if
 					length(Forks) == NumForksNeeded ->
 						eatingState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref);
@@ -200,30 +170,67 @@ sendForkRequest(Forks, [], _, _) ->
 		Forks;
 
 sendForkRequest(Forks, Neighbours, ClientPid, Ref) ->
-		io:format("Requesting forks ~n"),
+		io:format("Requesting forks ~p~n", [now()]),
 		FirstNeighbour  = hd(Neighbours),
 		RestNeighbours  = tl(Neighbours),
 		DoWeHaveFork 		= checkNeighbourInFork(Forks, FirstNeighbour),
 		if 
-			DoWeHaveFork ->
+			DoWeHaveFork -> % recursively get fork with rest of neighbours
 				sendForkRequest(Forks, RestNeighbours, ClientPid, Ref);
 			true -> % if we need the fork, request it and wait to receive it.
-							% in theory, we should eventually receive it without needing
-							% to worry about timeout or resending the request.
 				{philosopher, FirstNeighbour} ! {self(), requestFork},
 				receive 
 					{_, sendFork, Fork} -> % got fork
-						io:format("Received fork ~p ~n", [Fork]),
+						io:format("Received fork ~p ~p~n", [Fork, now()]),
 						sendForkRequest([Fork|Forks], RestNeighbours, ClientPid, Ref)
 					after ?TIMEOUT ->
 						sendForkRequest(Forks, Neighbours, ClientPid, Ref)
 				end
 		end.
 
+% Sends message to controller that made it hungry saying we are now eating.
+% Then waits for a stop_eating request or an identifyRequest.
+eatingState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> 
+		io:format("In eating state~p~n", [now()]),
+		DirtyForks = lists:map(fun(X) -> {element(1, X), element(2, X), false} end, Forks), % dirty forks
+
+		ClientPid ! {Ref, eating}, % sent to controller that transitioned hungry
+		receive
+			{ReceiverClientPid, identifyRequest} -> io:format("Identifying self as eating~p~n", [now()]),
+																			ReceiverClientPid ! {self(), identifyResponse, eating},
+																			eatingState(NumForksNeeded, DirtyForks, Neighbours, ClientPid, Ref);
+			{ReceiverClientPid, createFork} -> % see if we need to create the fork. We could get multiple such requests
+																 [NewNumForksNeeded, NewForks, NewNeighbours] = createFork(NumForksNeeded,
+																 																													 DirtyForks, Neighbours,
+																 																													 ReceiverClientPid),
+																 eatingState(NewNumForksNeeded, NewForks, NewNeighbours, ClientPid, Ref);
+			{ReceiverClientPid, ReceiverRef, leave} -> leavingState(NumForksNeeded, Forks, Neighbours, ReceiverClientPid, ReceiverRef);
+			{ReceiverClientPid, leaving} -> io:format("Received message that node ~p is leaving ~p~n", [node(ReceiverClientPid), now()]),
+																[NewNumForksNeeded, NewForks, NewNeighbours] = deleteForkFromPid(NumForksNeeded, Forks, Neighbours, ReceiverClientPid),
+																eatingState(NewNumForksNeeded, NewForks, NewNeighbours, ClientPid, Ref);
+			{_, requestFork} -> eatingState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref);
+			{_, _, stop_eating} -> io:format("No longer hungry~p~n", [now()]),
+														 thinkingState(NumForksNeeded, DirtyForks, Neighbours)
+		end.
+
+% NOTE: This requires the extra parameters ClientPid and Ref, as we must send
+% 			a message to the controller that sent its leave message, with matching refs,
+%				once we go to gone.
+leavingState(NumForksNeeded, _, Neighbours, ClientPid, Ref) -> io:format("got to leaving~p~n", [now()]),
+		lists:map(fun(X) -> {philosopher, X} ! {self(), leaving} end, Neighbours),
+		goneState(NumForksNeeded, [], Neighbours, ClientPid, Ref).
+
+% ClientPid and Ref required to send gone message back to controller that sent
+% leaving message.
+goneState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> io:format("got to gone~p~n", [now()]),
+										ClientPid ! {Ref, gone},
+										goneState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref).
+
+
 % Finds the fork with a given neighbour, returning it or False.
 findFork(Forks, Neighbour) ->
-		io:format("In findFork! ~n"),
-		io:format("Current forks and neighbour: ~p ~p~n", [Forks, Neighbour]),
+		io:format("In findFork ~p~n", [now()]),
+		io:format("Current forks and neighbour: ~p ~p~p~n", [Forks, Neighbour, now()]),
 		case lists:keyfind(Neighbour, 1, Forks) of
 			false ->
 				case lists:keyfind(Neighbour, 2, Forks) of
@@ -239,53 +246,21 @@ findFork(Forks, Neighbour) ->
 % returns a tuple if true, so we need the nested ifs to make it always
 % return a bool.
 checkNeighbourInFork(Forks, Neighbour) ->
-		io:format("In checkNeighbourInFork!~n"),
-		io:format("Forks are: ~p~n", [Forks]),
-		io:format("Neighbour is: ~p~n", [Neighbour]),
+		io:format("In checkNeighbourInFork ~p~n", [now()]),
 		if 
 			Forks == [] ->
 				false;
 			true ->
 				case lists:keyfind(Neighbour, 1, Forks) of
 					false -> 
-						io:format("keyfinds working?~n"),
 						case lists:keyfind(Neighbour, 2, Forks) of
 							false -> 
-								io:format("We should get here?~n"),
 								false;
 							_Else -> true
 						end;
 					_Else ->
 						true
 					end
-		end.
-
-% Sends message to controller that made it hungry saying we are now eating.
-% Then waits for a stop_eating request or an identifyRequest.
-eatingState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> io:format("got to eating~n"),
-		io:format("Before ~n"),
-		io:format("The forks are: ~p~n", [Forks]),
-		DirtyForks = lists:map(fun(X) -> {element(1, X), element(2, X), false} end, Forks), % dirty forks
-		io:format("After ~n"),
-
-		ClientPid ! {Ref, eating}, % sent to controller that transitioned hungry
-																						 % TODO: currently might be sent more than once.
-		receive
-			{ReceiverClientPid, identifyRequest} -> io:format("Identifying self as eating~n"),
-																			ReceiverClientPid ! {self(), identifyResponse, eating},
-																			eatingState(NumForksNeeded, DirtyForks, Neighbours, ClientPid, Ref);
-			{ReceiverClientPid, createFork} -> % see if we need to create the fork. We could get multiple such requests
-																 [NewNumForksNeeded, NewForks, NewNeighbours] = createFork(NumForksNeeded,
-																 																													 DirtyForks, Neighbours,
-																 																													 ReceiverClientPid),
-																 eatingState(NewNumForksNeeded, NewForks, NewNeighbours, ClientPid, Ref);
-			{ReceiverClientPid, ReceiverRef, leave} -> leavingState(NumForksNeeded, Forks, Neighbours, ReceiverClientPid, ReceiverRef);
-			{ReceiverClientPid, leaving} -> io:format("Received message that node ~p is leaving ~n", [node(ReceiverClientPid)]),
-																[NewNumForksNeeded, NewForks, NewNeighbours] = deleteForkFromPid(NumForksNeeded, Forks, Neighbours, ReceiverClientPid),
-																eatingState(NewNumForksNeeded, NewForks, NewNeighbours, ClientPid, Ref);
-			{_, requestFork} -> eatingState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref);
-			{_, _, stop_eating} -> io:format("No longer hungry~n"),
-																			thinkingState(NumForksNeeded, DirtyForks, Neighbours)
 		end.
 
 % Deletes the fork with clientPid, if it exists. If so, it also decrements
@@ -305,16 +280,3 @@ deleteForkFromPid(NumForksNeeded, Forks, Neighbours, ClientPid) ->
 			NewNeighbours = lists:delete(node(ClientPid), Neighbours),
 			[NumForksNeeded-1, Forks, NewNeighbours] % we didn't have fork, just return as is.
 		end.
-
-% NOTE: This requires the extra parameters ClientPid and Ref, as we must send
-% 			a message to the controller that sent its leave message, with matching refs,
-%				once we go to gone.
-leavingState(NumForksNeeded, _, Neighbours, ClientPid, Ref) -> io:format("got to leaving~n"),
-		lists:map(fun(X) -> {philosopher, X} ! {self(), leaving} end, Neighbours),
-		goneState(NumForksNeeded, [], Neighbours, ClientPid, Ref).
-
-% ClientPid and Ref required to send gone message back to controller that sent
-% leaving message.
-goneState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref) -> io:format("got to gone~n"),
-										ClientPid ! {Ref, gone},
-										goneState(NumForksNeeded, Forks, Neighbours, ClientPid, Ref).
--- readme.txt
+++ readme.txt
@@ -1,5 +1,19 @@
 Names: Eoin Nugent and Vijay Ramakrishnan
 
-The code is generally broken up into one state per function.
-There ar
+The code is generally broken up into one state per function, though with
+exceptions. For example, joinState is the only thing that ever sends
+identify messages, so we have left sendIdentifyMessage(..) directly
+below joinState. Messages are handled by each state, as required by the algorithm,
+within that state's function. The handling of messages is generally similar, but
+slightly different with each function as required by the algorithm.
 
+Some things are slightly less elegant than the ideal. Some erlang peculiarities
+(like having to pass parameters around everywhere and not being able to change
+the value of a given variable) made the code awkward in places. But in general
+the message receiving and mailbox metaphor greatly simplified the process for 
+that sort of thing.
+
+Implementing this, in general, was remarkably straightforward given the 
+complexity in terms of the algorithm, the fact that we're dealing with
+networked processes, and the total size of code. But it lent itself to
+pretty fast coding. 
